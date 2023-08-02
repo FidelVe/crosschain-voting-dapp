2ea769d33b1013412e41751030abda92700232c5
FidelVe <43641727+FidelVe@users.noreply.github.com>
2023-08-02 07:08:04 -0400

    added comments to java contract

--- contracts/jvm/VotingDapp/src/main/java/app/VotingDapp.java
+++ contracts/jvm/VotingDapp/src/main/java/app/VotingDapp.java
@@ -3,10 +3,7 @@ package app;
 import foundation.icon.score.client.ScoreClient;
 import score.Address;
 import score.Context;
-import score.DictDB;
-import score.UserRevertedException;
 import score.VarDB;
-import score.annotation.EventLog;
 import score.annotation.External;
 import score.annotation.Optional;
 import score.annotation.Payable;
@@ -22,6 +19,11 @@ public class VotingDapp {
     private final VarDB<String> destinationBtpAddress = Context.newVarDB("btpAddress", String.class);
     private final VarDB<Address> xcallContractAddress = Context.newVarDB("xcall", Address.class);
 
+    /*
+     * Constructor
+     * @param _sourceXCallContract: Address of the XCall contract
+     * @param _destinationBtpAddress: BTP address of the destination chain
+     */
     public VotingDapp(Address _sourceXCallContract, String _destinationBtpAddress) {
         this.destinationBtpAddress.set(_destinationBtpAddress);
         this.xcallContractAddress.set(_sourceXCallContract);
@@ -29,12 +31,21 @@ public class VotingDapp {
         this.countOfYes.set(BigInteger.ZERO);
     }
 
+    /*
+     * Send call message to XCall contract
+     * @param _data: Payload to be sent to XCall contract
+     * @param _rollback: Rollback payload to be sent to XCall contract
+     * @return: ID of the call message
+     */
     private BigInteger _sendCallMessage(byte[] _data, @Optional byte[] _rollback) {
         Address xcallSourceAddress = this.xcallContractAddress.get();
         String _to = this.destinationBtpAddress.get();
         return Context.call(BigInteger.class, Context.getValue(), xcallSourceAddress, "sendCallMessage", _to, _data, _rollback);
     }
 
+    /*
+     * Public method to vote Yes
+     */
     @Payable
     @External
     public void voteYes() {
@@ -51,6 +62,9 @@ public class VotingDapp {
         Context.println("sendCallMessage Response:" + id);
     }
 
+    /*
+     * Public method to vote No
+     */
     @Payable
     @External
     public void voteNo() {
@@ -67,6 +81,10 @@ public class VotingDapp {
         Context.println("sendCallMessage Response:" + id);
     }
 
+    /*
+     * Public method to get the current vote count
+     * @return: Map of vote counts
+     */
     @External(readonly = true)
     public Map<String, BigInteger> getVotes() {
         Map<String, BigInteger> votesMap = new HashMap<>();
@@ -75,17 +93,21 @@ public class VotingDapp {
         return votesMap;
     }
 
+    /*
+     * Public method to get the BTP address of the destination chain
+     * @return: BTP address of the destination chain
+     */
     @External(readonly = true)
     public String getDestinationBtpAddress() {
         return this.destinationBtpAddress.get();
     }
 
+    /*
+     * Public method to get the address of the XCall contract
+     * @return: Address of the XCall contract
+     */
     @External(readonly = true)
     public Address getXCallContractAddress() {
         return this.xcallContractAddress.get();
-    }
-
-    @External
-    public void handleCallMessage(String _from, byte[] _data) {
     }
 }
